7ab2c5bdca3763bcc27e9d935b830a21daa7eba6
Pujari-Raj <basvarajpujari607@gmail.com>
2023-03-01 23:37:08 +0530

    added login page and welcome page

--- js/script.js
+++ js/script.js
@@ -8,3 +8,26 @@ for (i = 0; i < menuLength; i++) {
         menuItem[i].className = "active";
     }
 }
+
+// Login functionality
+
+let email = "pujaribasvaraj018@gmail.com";
+let password  = "Pass@123";
+
+
+const Login = () => {
+    console.log('function called');
+    let user_email = document.getElementById("email").value;
+    let user_pass = document.getElementById("password").value;
+
+    console.log(user_email, user_pass);
+     if (user_email == email && user_pass == password) {
+        console.log('Login Credentials Matched');
+
+        window.location.href= 'index.html';
+     }   
+
+     else{
+        console.log('Invalid Login Credentials!!!!!');
+     }
+};
