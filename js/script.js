04148a7b8600b1f2398aadb4270d9c2dbc3eef1c
Pujari-Raj <basvarajpujari607@gmail.com>
2023-03-03 13:44:29 +0530

    added exam-results page with dynamic marks and grade

--- js/script.js
+++ js/script.js
@@ -12,7 +12,7 @@ for (i = 0; i < menuLength; i++) {
 // Login functionality
 
 let email = "Pujari Basvaraj";
-let password  = "Pass@123";
+let password = "Pass@123";
 
 
 const Login = () => {
@@ -21,20 +21,58 @@ const Login = () => {
     let user_pass = document.getElementById("password").value;
 
     console.log(user_email, user_pass);
-     if (user_email == email && user_pass == password) {
+    if (user_email == email && user_pass == password) {
         console.log('Login Credentials Matched');
         localStorage.setItem('username', user_email);
 
-        window.location.href= 'index.html';
-     }   
+        window.location.href = 'index.html';
+    }
 
     //  if (user_email.length == 0 && user_pass.length == 0) {
     //     document.querySelector('.email')
     //     document.querySelector('.password')
     //  }
 
-     else{
+    else {
         document.querySelector(".login_error").style.display = "block";
         console.log('Invalid Login Credentials!!!!!');
-     }
+    }
 };
+
+// Exam Grades Functionality
+
+function displayGrades() {
+    let subject = document.getElementById("student_subject").value;
+    let marks = document.getElementById("student_marks");
+    let grade = document.getElementById("student_grades");
+
+    console.log('display marks called!');
+    /* Subjects-> Grade
+     0-30- fail
+     30-45 -> B
+     45-60-> B+
+     60-75 -> A
+     75-100 -> A+
+  */
+    if (subject == "English") {
+        marks.value = 80;
+        grade.value = "A+";
+    } else if (subject == "Hindi") {
+        marks.value = 72;
+        grade.value = "A";
+    } else if (subject == "Mathematics") {
+        marks.value = 69;
+        grade.value = "A";
+    } else if (subject == "Science") {
+        marks.value = 74;
+        grade.value = "A";
+    } else if (subject == "Social-Studies") {
+        marks.value = 95;
+        grade.value = "A+";
+    }
+    else if (subject == "Physical-Activity") {
+        marks.value = 78;
+        grade.value = "A";
+    }
+}
+
